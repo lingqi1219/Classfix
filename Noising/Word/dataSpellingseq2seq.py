50bb6685ba4d61008772b9d50da0c704ceb40805
lingqi1219 <ziyxia1219@gmail.com>
2022-03-19 19:31:15 +0100

    Add files via upload

---
+++ Noising/Word/dataSpellingseq2seq.py
@@ -0,0 +1,285 @@
+import re
+import random
+import pickle
+from collections import Counter
+import string
+import tqdm
+
+
+
+
+ccode = []
+for files in range(5):
+	fileindex = str(files+16)
+	with open('../code/compileAbleCodePickle'+fileindex,'rb') as infile:
+		ccode = ccode + (pickle.load(infile))
+
+
+	
+extend_size = 6
+
+operations = ['add','rm','rm','ch','dup','dup']
+
+printable = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+insertToken = []
+
+types = ['int','short','long','byte','float','double','char','boolean','Integer']
+
+
+for p in printable:
+	insertToken.append(p)
+
+
+def tok_change(s):
+
+
+	de = random.randint(0,10)
+	
+	
+	if s.isnumeric():
+		return ""
+	if de > 4:
+		return ""
+	
+	if s in types:
+		if de == 1:
+			return random.choice(types)
+	
+	
+	rg = int(len(s)/2)
+
+	if rg < 2:
+		num = 1
+	else:
+		num = random.randint(1,rg)
+
+	for i in range(num):
+		l = len(s)
+		if l == 0:
+			s = s + random.choice(insertToken)
+			continue
+			
+		token_index = random.randint(0,l-1) 
+		op = random.choice(operations)
+		
+
+		if op == 'add':
+			s = s[:token_index] + random.choice(insertToken) + s[token_index:]
+
+		if op == 'dup':
+			s = s[:token_index] + s[token_index] + s[token_index:]
+		
+		if op == 'rm':
+			s = s[:token_index] + s[token_index+1:]
+
+		if op == 'ch':
+			s = s[:token_index] + random.choice(insertToken) + s[token_index+1:]
+
+		
+	return s
+
+def read_input(code):
+
+	code = re.sub(' +', ' ',code)
+	codeLines = code.split('\n')
+	
+	if len(codeLines) < 10:
+		return ([],[],[],[])
+	
+
+	variables = re.findall(r"[A-Za-z0-9]+", code)
+
+	if(len(variables) < 10):
+		return ([],[],[],[])
+
+	
+
+	lineToChange = []
+	for index,line in enumerate(codeLines):
+		invalid = re.findall(r"[\*]|[*/]|[//]|[@]", line)
+		if len(invalid)>= 1:
+			continue	
+			
+		listline = re.findall(r"[A-Za-z]+|[0-9]+|\t|\S| |.", str(line))
+		if len(listline) > 100:
+			continue	
+		
+		for tok in listline:
+			if tok.isalnum():
+				lineToChange.append(index)
+				break
+
+
+	if len(lineToChange) < 1:
+		return ([],[],[],[])
+		
+	
+
+	linesToBreak = random.choices(lineToChange,k=extend_size)
+	
+	x4 = []
+	x10 = []
+	x20 = []
+	y = []
+	for line in linesToBreak:
+		current = codeLines.copy()[line]
+		
+		listline = re.findall(r"[A-Za-z]+|[0-9]+|\t|\S| |.", current)
+			
+		tokToChange = []
+		for index,tok in enumerate(listline):
+			if tok.isalnum():
+				tokToChange.append(index)
+		
+		
+		tid = random.choice(tokToChange)
+		listline[tid] = tok_change(listline[tid])
+		de = random.randint(0,20)
+		if de > 19:
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+		elif de > 18:
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+		elif de > 17:
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+		
+		current = "".join(listline)	
+
+
+		
+
+		leftLines = []
+		rightLines = []
+		errorline = "<E>" + current
+		for i in range(10):
+			leftLines.append("")
+			rightLines.append("")
+		
+		for i in range(10):
+			if (line - 1 - i >= 0):
+				leftLines[i] = "<L" + str(i) +">" +  codeLines[line - 1 - i] 
+		for i in range(10):
+			if (line + 1 + i < len(codeLines)):
+				rightLines[i] = "<R" + str(i) + ">" + codeLines[line + 1 + i]
+
+		leftLines.reverse()
+
+		n20 = "\n".join(leftLines+[errorline]+rightLines)
+		n10 = "\n".join(leftLines[-5:]+[errorline]+rightLines[0:5])	
+		n4 = "\n".join(leftLines[-2:]+[errorline]+rightLines[0:2])
+
+		x4.append(n4)
+		x10.append(n10)
+		x20.append(n20)
+		y.append(codeLines.copy()[line])
+	
+
+	return (x4,x10,x20,y)
+	
+train_x4 = []
+train_x10 = []
+train_x20 = []
+train_y = []
+
+
+	
+for code in tqdm.tqdm(ccode):
+
+	(x4,x10,x20,yi) = read_input(str(code))
+
+	train_x4.extend(x4)
+	train_x10.extend(x10)
+	train_x20.extend(x20)
+	train_y.extend(yi)
+	
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+for i in range(100):
+	print(train_x4[i])
+	print()
+	print(train_x10[i])
+	print()
+	print(train_x20[i])
+	print()
+	print(train_y[i])
+	print("------------------------------------------")
+
+code20 = train_x20
+line = train_y
+fast = []
+for i,c in enumerate(code20):
+        if b'\x00' in bytes(line[i],'utf-8') or b'\x00' in bytes(code20[i],'utf-8') :
+                continue
+        fast.append([str(code20[i]),str(line[i])])
+
+
+df = pd.DataFrame(fast,columns=['input_text','target_text'])
+df['input_text'].astype(str)
+df['target_text'].astype(str)
+df = df.dropna()
+df = df.sample(frac = 1)
+df.to_csv('wordseq20_4.csv')
+
+
+
+exit()
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------
+code4 = train_x4
+line = train_y
+
+
+fast = []
+for i,c in enumerate(code4):
+        if b'\x00' in bytes(line[i],'utf-8') or b'\x00' in bytes(code4[i],'utf-8') :
+                continue
+        fast.append([str(code4[i]),str(line[i])])
+
+
+df = pd.DataFrame(fast,columns=['input_text','target_text'])
+df['input_text'].astype(str)
+df['target_text'].astype(str)
+df = df.dropna()
+df = df.sample(frac = 1)
+#train,valid = train_test_split(df,test_size=0.05)
+df.to_csv('wordseq4_3.csv')
+
+
+#-------------------------------------------------------------------------------------
+
+code10 = train_x10
+line = train_y
+fast = []
+for i,c in enumerate(code10):
+        if b'\x00' in bytes(line[i],'utf-8') or b'\x00' in bytes(code10[i],'utf-8') :
+                continue
+        fast.append([str(code10[i]),str(line[i])])
+
+
+df = pd.DataFrame(fast,columns=['input_text','target_text'])
+df['input_text'].astype(str)
+df['target_text'].astype(str)
+df = df.dropna()
+df = df.sample(frac = 1)
+df.to_csv('wordseq10_3.csv')
+
+#-------------------------------------------------------------------------------------
+
+	
---
+++ Noising/Word/newClass.py
@@ -0,0 +1,248 @@
+import re
+import random
+import pickle
+from collections import Counter
+import string
+import tqdm
+
+
+existingcount = Counter(list(range(0, 201)))
+
+
+ccode = []
+for files in range(20):
+	fileindex = str(files+1)
+	with open('../code/compileAbleCodePickle'+fileindex,'rb') as infile:
+		ccode = ccode + (pickle.load(infile))
+
+#count = Counter([])
+#for i in range(20):
+#	fileindex = str(i+1)
+#	with open('../code/mostFrequentPickle'+fileindex,'rb') as infile:
+#		mf = pickle.load(infile)
+#		count = count + mf
+	
+#mfm = count.most_common(500)
+#mfmt = []
+#for (token,counts) in mfm:
+#	if token.isalpha() and len(token)>2:
+#		mfmt.append(token)
+#mfmt = mfmt[0:300]
+#print(mfmt)
+	
+extend_size = 10
+
+operations = ['add','rm','rm','ch','dup','dup']
+
+printable = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.'
+
+types = ['int','short','long','byte','float','double','char','boolean','final','class','void','new','Integer','interface','false','true','if','else','abstract']
+insertToken = []
+
+
+for p in printable:
+	insertToken.append(p)
+
+
+def tok_change(s):
+	
+
+	de = random.randint(0,5)
+
+	if de > 1 or s.isnumeric():
+		s = ""
+		return s
+	if s in types:
+		if de == 1:
+			return random.choice(types)
+
+			
+
+	rg = int(len(s)/2-1)
+
+	if rg < 2:
+		num = 1
+	else:
+		num = random.randint(1,rg)
+
+	for i in range(num):
+		l = len(s)
+		if l == 0:
+			s = s + random.choice(insertToken)
+			continue
+			
+		token_index = random.randint(0,l-1) 
+		op = random.choice(operations)
+		
+
+		if op == 'add':
+			s = s[:token_index] + random.choice(insertToken) + s[token_index:]
+
+		if op == 'dup':
+			s = s[:token_index] + s[token_index] + s[token_index:]
+		
+		if op == 'rm':
+			s = s[:token_index] + s[token_index+1:]
+
+		if op == 'ch':
+			s = s[:token_index] + random.choice(insertToken) + s[token_index+1:]
+
+		
+	return s
+
+def read_input(code,maxindex):
+
+	variables = re.findall(r"[A-Za-z]+", code)
+	if(len(variables) < 10):
+		return ([],[])
+
+	codeLines = code.split('\n')
+	
+	if len(codeLines)>200:
+		return ([],[])
+	
+	
+	lineToChange = []
+	for index,line in enumerate(codeLines):
+		listline = re.findall(r"[A-Za-z]+|\t|\S| |.", line)
+		if len(listline) > 50:
+			return([],[])		
+			
+		for tok in listline:
+			if tok.isalpha():
+				lineToChange.append(index)
+				break
+	
+	if len(lineToChange) < extend_size:
+		return ([],[])
+		
+	
+	
+	linesToBreak = random.sample(lineToChange,extend_size)
+
+
+	
+	x = []
+	yline = []
+
+
+#	for i in range(len(codeLines)):
+#		codeLines[i] = "<"+str(i)+">"+codeLines[i]
+
+	for line in linesToBreak:
+		current = codeLines[line]
+		listline = re.findall(r"[A-Za-z0-9]+|\t|\S| |.", current)
+			
+		tokToChange = []
+		for index,tok in enumerate(listline):
+			if tok.isalnum():
+				tokToChange.append(index)
+		
+		
+		tid = random.choice(tokToChange)
+		listline[tid] = tok_change(listline[tid])
+		de = random.randint(0,10)
+		if de >= 9:
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+
+		elif de >= 8:
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+		elif de >= 7:
+			tid = random.choice(tokToChange)
+			listline[tid] = tok_change(listline[tid])
+	
+
+		for i in range(len(codeLines)):
+			codeLines[i] = "<"+str(i)+">"+codeLines[i]
+		
+		current = "<" + str(line) +">" +  "".join(listline)		
+
+		m = maxindex
+		if m == 20:
+			m = random.randint(0,19)
+			yline.append(20)
+			current = codeLines[line]
+
+		xleft = line - m
+		xright = line + (20-m)
+
+ 		
+
+		if xleft < 0:
+			e = codeLines[0:line] + [current] + codeLines[line+1:xright]
+			e = ["<L>" for i in range(20-len(e))] + e 
+		else:
+			e = codeLines[xleft:line] + [current] + codeLines[line+1:xright]
+
+
+		if xright > len(codeLines):
+			e = e + ["<R>" for i in range(20-len(e))] 
+
+		x.append("\n".join(e))
+
+		if not maxindex == 20:
+			for i in range(20):
+				if e[i] == current:
+					yline.append(str(i))
+					break
+		
+	if not len(yline) == len(x):
+		exit()
+	return (x,yline)
+	
+train_x = []
+train_yline = []
+maxindex = 15
+
+prefix = 1	
+for code in tqdm.tqdm(ccode):
+
+	if(prefix%400000 == 0):
+		with open('../newdata/new_trainx_'+str(int(prefix/400000)),'wb') as cap:
+			pickle.dump(train_x,cap)			 
+		with open('../newdata/new_trainyline_'+str(int(prefix/400000)),'wb') as cac:
+			pickle.dump(train_yline,cac)
+
+		print(len(train_x))
+		print(len(train_yline))
+		train_x = []
+		train_yline = []
+
+	prefix = prefix + 1	
+
+
+	
+	(xi,ylinei) = read_input(code,maxindex)
+	
+	if maxindex >= 20:
+		maxindex = 0
+	else:
+		maxindex = maxindex + 1
+
+	train_x.extend(xi)
+	train_yline.extend(ylinei)
+
+	
+		
+with open('../newdata/new_trainx_f','wb') as cap:
+	pickle.dump(train_x,cap)
+with open('../newdata/new_trainyline_f','wb') as cac:
+	pickle.dump(train_yline,cac)
+
+
+for i in range(100):
+	print(train_yline[i])
+	print(train_x[i])
+	print()
+
+print(len(train_x))
+
+
